c3a675027ff2a6604db119883373c61f8ace78d5
AlejaH1213 <alejajara2@gmail.com>
2023-11-19 14:52:23 -0800

    created grid and styled it to be in the center

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,15 +1,23 @@
 class MainController < ApplicationController
-    # The index method is the responsible for setting up the initial state of the game
-    def index
-        # These are the clues for the game 
-        @clues = [
-            "look in the table",
-            "open the drawer",
-            "read the notebook"
-        ]
-        # This variable is keeping track of the index of the current clue
-        @current_clue_index = 0
-        #this variable holds the content of the current clue, its initialized with the first clue of the array
-        @current_clue = @clues[@current_clue_index]
-    end
+  before_action :initialize_game, only: [:index, :reveal]
+  # The index method is the responsible for setting up the initial state of the game
+  def index
+    render 'index'
+  end
+  # this method is responsible for setting up the initial state of the game 
+  def initialize_game
+    # variable to store the current state of the game 
+    @game_state = initialize_grid(5)
+  end
+  # this method is responsible for generating  the intial grid for the game 
+  def initialize_grid(size)
+    Array.new(size) {Array.new(size,{ type: :blank, revealed: false})}
+  end
+  # this method handles the user click in one of the grids
+  def reveal
+    row = params[:row].to_i
+    col = params[:col].to_i
+
+    render json: {status: 'success', game_state: @game_state}
+  end
 end
